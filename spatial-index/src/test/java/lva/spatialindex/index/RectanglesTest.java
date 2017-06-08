7c77049b2ff59a8d4f149cd2ecf40c2e80b6c84b
VLitvinenko <viktor.a.litvinenko@gmail.com>
2017-06-09 00:19:42 +0300

    add tests for Rectangles

---
+++ spatial-index/src/test/java/lva/spatialindex/index/RectanglesTest.java
@@ -0,0 +1,25 @@
+package lva.spatialindex.index;
+
+import org.junit.Test;
+
+import java.awt.Rectangle;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author vlitvinenko
+ */
+public class RectanglesTest {
+    @Test
+    public void test_area() {
+        assertEquals(0, Rectangles.area(new Rectangle()));
+        assertEquals(8, Rectangles.area(new Rectangle(0, 0, 2, 4)));
+    }
+
+    @Test
+    public void test_margin() {
+        assertEquals(0, Rectangles.margin(new Rectangle()));
+        assertEquals(12, Rectangles.margin(new Rectangle(0, 0, 2, 4)));
+    }
+
+}
