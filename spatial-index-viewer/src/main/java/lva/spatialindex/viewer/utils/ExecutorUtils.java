a587083aedd7054e9e933f54b95221236bf0cd1f
vlitvinenko <viktor.a.litvinenko@gmail.com>
2021-10-20 01:57:52 +0300

    use coroutines for repository building

--- spatial-index-viewer/src/main/java/lva/spatialindex/viewer/utils/ExecutorUtils.java
+++ spatial-index-viewer/src/main/java/lva/spatialindex/viewer/utils/ExecutorUtils.java
@@ -2,13 +2,9 @@ package lva.spatialindex.viewer.utils;
 
 import com.google.common.util.concurrent.MoreExecutors;
 
-import java.util.Collection;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * @author vlitvinenko
@@ -21,10 +17,4 @@ public class ExecutorUtils {
             MoreExecutors.shutdownAndAwaitTermination(EXECUTOR_SERVICE, 10, TimeUnit.SECONDS);
         }));
     }
-
-    public static <R> Collection<R> gather(Collection<CompletableFuture<R>> asyncResults) {
-        return asyncResults.stream().map(CompletableFuture::join)
-                .collect(toList());
-    }
-
 }
