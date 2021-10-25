529f2b293f2597258c9ce0b3d67b00d7d869bca9
vlitvinenko <viktor.a.litvinenko@gmail.com>
2021-10-25 19:05:25 +0300

    refactoring: separate data with metadata

--- spatial-index/src/main/java/lva/spatialindex/index/Entry.kt
+++ spatial-index/src/main/java/lva/spatialindex/index/Entry.kt
@@ -1,121 +1,86 @@
-package lva.spatialindex.index;
+package lva.spatialindex.index
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.Serializer;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
-import io.vavr.control.Either;
-import lombok.EqualsAndHashCode;
-import lva.spatialindex.storage.Storage;
-
-import java.awt.Rectangle;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.Serializer
+import com.esotericsoftware.kryo.io.Input
+import com.esotericsoftware.kryo.io.Output
+import io.vavr.control.Either
+import lva.spatialindex.storage.Storage
+import java.awt.Rectangle
+import java.util.*
 
 /**
  * @author vlitvinenko
  */
-@EqualsAndHashCode(exclude = {"storage"})
-public
-class Entry {
-    static final int SIZE = 24;
+internal class Entry(private val storage: Storage<Node>, mbr: Rectangle, childOffset: Long) {
+    private data class Body(var mbr: Rectangle, val childOffset: Long)
+    private val body = Body(mbr, childOffset)
 
-    static final Comparator<Entry> LEFT_TO_RIGHT_BY_LEFT_COMPARATOR =
-            Comparator.comparingInt(e -> e.mbr.x);
+    var mbr: Rectangle by body::mbr
+    val childOffset: Long by body::childOffset
 
-    static final Comparator<Entry> LEFT_TO_RIGHT_BY_RIGHT_COMPARATOR =
-            Comparator.comparingInt(e -> e.mbr.x + e.mbr.width);
+    fun data(): Either<Node, Long> = if (childOffset < 0)
+        Either.right(childOffset) else Either.left(storage.read(childOffset))
 
-    static final Comparator<Entry> TOP_TO_BOTTOM_BY_BOTTOM_COMPARATOR =
-            Comparator.comparingInt(e -> e.mbr.y + e.mbr.height);
+    val childNode: Optional<Node>
+        get() = data().swap().toJavaOptional()
 
-    static final Comparator<Entry> TOP_TO_BOTTOM_BY_TOP_COMPARATOR =
-            Comparator.comparingInt(e -> e.mbr.y);
+    val isLeaf
+        get() = childOffset < 0
 
-    static final Collection<Comparator<Entry>> X_COMPARATORS =
-            Arrays.asList(LEFT_TO_RIGHT_BY_LEFT_COMPARATOR, LEFT_TO_RIGHT_BY_RIGHT_COMPARATOR);
+    override fun equals(other: Any?) = if (other is Entry) body == other.body else false
+    override fun hashCode() = body.hashCode()
 
-    static final Collection<Comparator<Entry>> Y_COMPARATORS =
-            Arrays.asList(TOP_TO_BOTTOM_BY_BOTTOM_COMPARATOR, TOP_TO_BOTTOM_BY_TOP_COMPARATOR);
-
-    private final Storage<Node> storage;
-    private final long childOffset;
-    private Rectangle mbr; // TODO: think about to make final
-
-
-    Entry(Storage<Node> storage, Rectangle mbr, long childOffset) {
-        this.storage = storage;
-        this.mbr = mbr;
-        this.childOffset = childOffset;
-    }
-
-    Either<Node, Long> data() {
-        return childOffset < 0 ? Either.right(childOffset) :
-                Either.left(storage.read(childOffset));
-    }
-
-    Optional<Node> getChildNode() {
-        return data().swap().toJavaOptional();
-    }
-
-    long getChildOffset() {
-        return childOffset;
-    }
-
-    Rectangle getMbr() {
-        return mbr;
-    }
-
-    void setMbr(Rectangle mbr) {
-        this.mbr = mbr;
-    }
-
-    boolean isLeaf() {
-        return childOffset < 0;
-    }
-
-    static Rectangle union(List<Entry> entries) {
-        return entries.stream()
-                .map(Entry::getMbr).reduce(Rectangle::union)
-                .orElse(new Rectangle());
-    }
-
-    static int margin(List<Entry> entries) {
-        return entries.stream()
-                .mapToInt(e -> Rectangles.margin(e.mbr))
-                .sum();
-    }
-
-    static class Ser extends Serializer<Entry> {
-        private final Storage<Node> storage;
-        Ser(Storage<Node> storage) {
-            this.storage = storage;
+    internal class Ser(private val storage: Storage<Node>) : Serializer<Entry>() {
+        override fun write(kryo: Kryo, output: Output, entry: Entry) = with(output) {
+            val mbr = entry.mbr
+            writeInt(mbr.x)
+            writeInt(mbr.y)
+            writeInt(mbr.width)
+            writeInt(mbr.height)
+            writeLong(entry.childOffset)
         }
 
-        @Override
-        public void write(Kryo kryo, Output output, Entry entry) {
-            Rectangle mbr = entry.getMbr();
-
-            output.writeInt(mbr.x);
-            output.writeInt(mbr.y);
-            output.writeInt(mbr.width);
-            output.writeInt(mbr.height);
-            output.writeLong(entry.getChildOffset());
+        override fun read(kryo: Kryo, input: Input, type: Class<Entry>): Entry = with(input) {
+            val x = readInt()
+            val y = readInt()
+            val width = readInt()
+            val height = readInt()
+            val childOffset = readLong()
+            Entry(storage, Rectangle(x, y, width, height), childOffset)
         }
+    }
 
-        @Override
-        public Entry read(Kryo kryo, Input input, Class<Entry> type) {
-            int x = input.readInt();
-            int y = input.readInt();
-            int width = input.readInt();
-            int height = input.readInt();
-            long childOffset = input.readLong();
+    companion object {
+        const val SIZE = 24
 
-            return new Entry(storage, new Rectangle(x, y, width, height), childOffset);
-        }
+        @JvmField
+        val LEFT_TO_RIGHT_BY_LEFT_COMPARATOR: Comparator<Entry> = Comparator.comparingInt { it.mbr.x }
+
+        @JvmField
+        val LEFT_TO_RIGHT_BY_RIGHT_COMPARATOR: Comparator<Entry> = Comparator.comparingInt { it.mbr.x + it.mbr.width }
+
+        @JvmField
+        val TOP_TO_BOTTOM_BY_BOTTOM_COMPARATOR: Comparator<Entry> = Comparator.comparingInt { it.mbr.y + it.mbr.height }
+
+        @JvmField
+        val TOP_TO_BOTTOM_BY_TOP_COMPARATOR: Comparator<Entry> = Comparator.comparingInt { it.mbr.y }
+
+        @JvmField
+        val X_COMPARATORS: Collection<Comparator<Entry>> =
+            listOf(LEFT_TO_RIGHT_BY_LEFT_COMPARATOR, LEFT_TO_RIGHT_BY_RIGHT_COMPARATOR)
+
+        @JvmField
+        val Y_COMPARATORS: Collection<Comparator<Entry>> =
+            listOf(TOP_TO_BOTTOM_BY_BOTTOM_COMPARATOR, TOP_TO_BOTTOM_BY_TOP_COMPARATOR)
+
+        @JvmStatic
+        fun union(entries: List<Entry>): Rectangle =
+            entries.asSequence().map { it.mbr }
+                .reduceOrNull { acc, r -> acc.union(r) } ?: Rectangle()
+
+        @JvmStatic
+        fun margin(entries: List<Entry>): Int =
+            entries.asSequence().map { Rectangles.margin(it.mbr) }.sum()
     }
 }
