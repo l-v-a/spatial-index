c46e98151e70fa41634bb6b2f5c0fcadf35a36f8
vlitvinenko <viktor.a.litvinenko@gmail.com>
2021-10-26 14:28:01 +0300

    refactoring

--- spatial-index/src/main/java/lva/spatialindex/index/Distributions.kt
+++ spatial-index/src/main/java/lva/spatialindex/index/Distributions.kt
@@ -7,28 +7,24 @@ import lva.spatialindex.index.Entry.Companion.margin
  */
 
 internal typealias GroupPair = Pair<List<Entry>, List<Entry>>
-internal typealias DistributionGroups = List<GroupPair>
 
-internal fun getGroupMargins(distributionGroups: List<DistributionGroups>): Int =
+internal fun getGroupMargins(distributionGroups: List<List<GroupPair>>): Int =
     distributionGroups.sumOf { margins(it) }
 
-internal fun getDistributionGroups(entries: List<Entry>, comparators: Collection<Comparator<Entry>>): List<DistributionGroups> =
+internal fun getDistributionGroups(entries: List<Entry>, comparators: Collection<Comparator<Entry>>): List<List<GroupPair>> =
     comparators.asSequence()
         .map { entries.sortedWith(it) }
         .map { distributions(it) }
         .toList()
 
-private fun margins(groups: DistributionGroups): Int =
+private fun margins(groups: List<GroupPair>): Int =
     groups.sumOf { margin(it.first) + margin(it.second) }
 
-private fun distributions(entries: List<Entry>): DistributionGroups {
-    val groups = arrayListOf<GroupPair>()
-    for (k in 0 until Node.MAX_ENTRIES - 2 * Node.MIN_ENTRIES + 2) {
-        val pair = GroupPair(
+private fun distributions(entries: List<Entry>): List<GroupPair> {
+    return (0 until Node.MAX_ENTRIES - 2 * Node.MIN_ENTRIES + 2).map { k ->
+        GroupPair(
             entries.subList(0, Node.MIN_ENTRIES + k),
             entries.subList(Node.MIN_ENTRIES + k, entries.size)
         )
-        groups.add(pair)
-    }
-    return groups
+    }.toList()
 }
