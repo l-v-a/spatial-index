3c33779911acbeae08c4f1401597aace972c5c68
vlitvinenko <viktor.a.litvinenko@gmail.com>
2021-11-04 03:49:02 +0300

    simplify

--- spatial-index/src/main/kotlin/lva/spatialindex/index/Node.kt
+++ spatial-index/src/main/kotlin/lva/spatialindex/index/Node.kt
@@ -12,8 +12,8 @@ internal class Node(private val storage: Storage<Node>, var offset: Long) {
     val isLeaf get() = entries.firstOrNull()?.isLeaf ?: true
     val isFull get() = entries.size >= MAX_ENTRIES
     var mbr: Rectangle = NULL_RECTANGLE
-        get() = when {
-            field.isNull -> entries.union().also { field = it }
+        get() = when(field) {
+            NULL_RECTANGLE -> entries.union().also { field = it }
             else -> field
         }
         private set
@@ -51,6 +51,8 @@ internal class Node(private val storage: Storage<Node>, var offset: Long) {
     }
 
     companion object {
+        private val NULL_RECTANGLE = Rectangle()
+
         private const val PAGE_SIZE = 4096
         const val MAX_ENTRIES = PAGE_SIZE / Entry.SIZE - 1
         const val MIN_ENTRIES = MAX_ENTRIES * 2 / 5
@@ -59,6 +61,3 @@ internal class Node(private val storage: Storage<Node>, var offset: Long) {
             Node(storage, -1).also { storage.add(it) }
     }
 }
-
-private val NULL_RECTANGLE = Rectangle()
-private val Rectangle.isNull: Boolean get() = this === NULL_RECTANGLE
