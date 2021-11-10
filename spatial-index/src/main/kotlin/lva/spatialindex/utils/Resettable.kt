22745868bf7b9ff9c2b424d8fe0554f920e53e31
vlitvinenko <viktor.a.litvinenko@gmail.com>
2021-11-10 08:44:11 +0300

    simplify

--- spatial-index/src/main/kotlin/lva/spatialindex/utils/Resettable.kt
+++ spatial-index/src/main/kotlin/lva/spatialindex/utils/Resettable.kt
@@ -1,24 +1,25 @@
 package lva.spatialindex.utils
 
+import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 /**
  * @author vlitvinenko
  */
 
-fun <T: Any> resettableWith(uninitializedValue: T, init: () -> T) = Resettable(uninitializedValue, init)
+fun <V: Any> resettableWith(uninitializedValue: V, init: () -> V) = Resettable(uninitializedValue, init)
 
-class Resettable<T: Any>(private val uninitializedValue: T, private val init: () -> T) {
-    private var value: T = uninitializedValue
+class Resettable<V: Any>(private val uninitializedValue: V, private val init: () -> V) : ReadWriteProperty<Any, V> {
+    private var value: V = uninitializedValue
 
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+    override operator fun getValue(thisRef: Any, property: KProperty<*>): V {
         if (value === uninitializedValue) {
             value = init()
         }
         return value
     }
 
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, newValue: T) {
-        value = newValue
+    override operator fun setValue(thisRef: Any, property: KProperty<*>, value: V) {
+        this.value = value
     }
 }
